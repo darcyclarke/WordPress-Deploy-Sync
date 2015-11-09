0c8ffbe5359825e01b86f682e305be1d6ae64a0c
Darcy Clarke <darcy@darcyclarke.me>
2015-11-09 01:48:09 -0500

    Fix

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
-# Gulp WordPress Deploy
+# Gulp WordPress Deploy & Sync
 
-A WordPress Deployment workflow that doesn't kill braincells
+A WordPress Deployment & Sync workflow that **doesn't kill braincells**
 
 ### Table of Contents
 
@@ -11,15 +11,15 @@ A WordPress Deployment workflow that doesn't kill braincells
 
 ### Installation
 
-#### Install Dependancies:
+#### Install **wordpress-deploy-sync**:
 
 ```bash
-npm install
+npm install wordpress-deploy-sync --save
 ````
 
-#### Update `config.json`:
+#### Create a `config.json` file:
 
-The configuration file holds all the relevant database, user and path information.
+The configuration file holds all the relevant database, user and path information. You will have to create one of these in the root of your theme folder (a default example of the schema is below).
 
 ```json
 {
@@ -40,25 +40,62 @@ The configuration file holds all the relevant database, user and path informatio
     "db_host": "localhost",
     "port": 3306,
     "ssh": {
+      "theme_name": "your_theme_directory"
       "hostname": "ftp.domain.com",
       "username": "ftp_username",
       "password": "ftp_password",
-      "destination": "./public_html/",
+      "destination": "./public_html/wordpress/",
       "port": 21
     }
   }
 }
 ````
 
+**Note:** Ensure that you add `config.json` to your `.gitignore` as it's likely to have private credentials you don't want exposed publically or checked into a repo.
+
 **Note:** Don't add trailing slashes to `local.url` or `remote.url` values.
 
 **Note:** You shouldn't have to update values like `local.port` or `local.dump` unless you've changed where [MAMP]() has been installed or is running the MySQL server.
 
-**Note:** The `remote.ssh.desination` value should be the path to your theme directory on your server.
+**Note:** The `remote.ssh.desination` value should be the path to the root of your WordPress install is on your server.
+
+#### Enable Remote MySQL Access:
+
+For many hosting provides (like **BlueHost**) you will have to enable remote MySQL access. If you have to provide your IP address to *whitelist* you can use a service like [whatsmyip.org](http://www.whatsmyip.org/) or run the command `ifconfig` in a terminal session.
+
+### Initialization
+
+`wordpress-deploy-sync` expects config options to be passed to it in order for it to find and map everything correctly. Using a file like the `config.json` example above is the easiest way to go.
+
+```javascript
+var config      = require( 'config' );
+var wordpress   = require( 'wordpress-deploy-sync' );
+wordpress( config ).deploy();
+````
+
+#### Useage with Gulp:
+
+`wordpress-deploy-sync` can be used on it's own or with a task runner. An example of how you can integrate it your `gulpfile.js` is below:
+
+```javascript
+var gulp        = require( 'gulp' );
+var config      = require( 'config' );
+var wordpress   = require( 'wordpress-deploy-sync' )( config );
+
+gulp.task( 'deploy', function () {
+  wordpress.deploy();
+});
+
+gulp.task( 'sync', function () {
+  wordpress.sync();
+});
+````
 
 ### Deploying
 
-Gulp WordPress Deploy will generate a `./sync` folder in your theme directory. This will store things like a backup copy of your local and remote databases as well as any content from your `wp-contents/plugins` or `wp-contents/uploads` folders. These copies are required when running a `gulp sync` which is perfect for working with a team over Github (as you will need to check in this content to stay consistent).
+Gulp WordPress Deploy & Sync will generate a `./sync` folder in your theme directory. This will store things like a backup copy of your local and remote databases as well as any content from your `wp-contents/plugins` or `wp-contents/uploads` folders. These copies are required when running a `gulp sync` which is perfect for working with a team over Github (as you will need to check in this content to stay consistent).
+
+**Note:** You will need to be running MAMP/WAMP etc. to properly run `deploy`.
 
 ```bash
 gulp deploy
@@ -67,6 +104,8 @@ gulp deploy
 ### Syncing
 
 When you're working with a team of people on a WordPress you'll want to check in code. Before doing this, you should run `gulp deploy` to ensure that all relevant *static assets* and your local database will be reflected in your Git repo. If you've pulled down someones work (ie. `git pull`) and they've made changes to the WordPress database, plugins or uploads, then it's a good time to run `gulp sync`.
+
+**Note:** You will need to be running MAMP/WAMP etc. to properly run `sync`.
 
 ```bash
 gulp sync
--- gulpfile.js
+++ gulpfile.js
@@ -3,165 +3,22 @@
 // Setup
 // --------------------------------------
 
-var fs              = require( 'fs' );
-var gulp            = require( 'gulp' );
-var plugins         = require( 'gulp-load-plugins' )();
-var runsequence     = require( 'run-sequence' );
-var mysql           = require( 'mysql' );
-var mkpath          = require( 'mkpath' );
-var exec            = require( 'child_process' ).exec;
-var wrench          = require( 'wrench' );
-var colors          = require( 'colors' );
+var gulp      = require( 'gulp' );
+var config    = require( './config' );
+var wordpress = require( 'main' )( config );
 
 // --------------------------------------
-// Configuration
-// --------------------------------------
-
-var config          = require( './config' );
-
-// --------------------------------------
-// Environment
-// --------------------------------------
-// Local: gulp ...
-// Development: NODE_ENV=dev gulp ...
-// Staging: NODE_ENV=staging gulp ...
-// Production: $ NODE_ENV=production gulp ...
-// --------------------------------------
-
-var env             = process.env.NODE_ENV || 'local';
-
-// --------------------------------------
-// Paths
-// --------------------------------------
-
-var paths = {
-  content: '../../',
-  watch: '**/*.{js,css,jpeg,jpg,png,gif}'
-};
-
-// --------------------------------------
-// Sync Static Assets
-// --------------------------------------
-
-gulp.task( 'statics', function () {
-
-  gulp.src( paths.content + '**/*' )
-    .pipe(plugins.rsync({
-      root: paths.content,
-      hostname: config.remote.ssh.hostname,
-      destination: config.remote.ssh.destination,
-      username: ( config.remote.ssh.username ) ? config.remote.ssh.username : null,
-      port: config.remote.ssh.port,
-      recursive: true,
-      clean: true
-    }));
-
-});
-
-// --------------------------------------
-// Database Sync
-// - export remote database
-// - export local database
-// - find/replace local database with remote database config
-// - create 2 temporary databases for remote / local (transformed) databases
-// - do a comparison of the 2 databases
-// - save the diff that is returned
-// - drop both databases
-// - apply diff to remote database
-// - sync assets
+// Deploy Task
 // --------------------------------------
 
 gulp.task( 'deploy', function () {
-
-  var local_dump = './sync/local_backup.sql';
-
-  // Create dump command
-  var local_dump_cmd = config.local.dump + ' -u ' + config.local.db_user + ' --password=' + config.local.db_password + ' ' + config.local.db_name + ' > ' + local_dump;
-
-  // Create ./sync/ folders
-  mkpath.sync('./sync');
-
-  // --------------------------------------
-  // Connect to Local Database
-  // --------------------------------------
-
-  var local = mysql.createConnection({
-    host: config.local.db_host,
-    user: config.local.db_user,
-    password: config.local.db_password,
-    database: config.local.db_name,
-    port: config.local.port
-  });
-
-  local.connect( function( err ) {
-
-    // Handle Errors
-    if( err ) {
-      console.log( err );
-      console.log('Error connecting to DB');
-      return;
-    }
-
-    // Log connection
-    console.log('Connection established to local database:'.green, config.local.db_name.yellow );
-
-    // Run local dump
-    exec( local_dump_cmd,  function( err, out, code ) {
-
-      // Replace local config with sync options
-      var contents = fs.readFileSync( local_dump, 'utf-8' );
-      contents = contents.replace( config.local.url, 'wordpress-deploy-sync' );
-      fs.writeFileSync( local_dump, contents );
-
-      // Clone relevant wp-contents/ folders
-      wrench.copyDirSyncRecursive('../../plugins/', './sync/plugins/', { forceDelete: true });
-      wrench.copyDirSyncRecursive('../../uploads/', './sync/uploads/', { forceDelete: true });
-
-      // Setup connection to remote database
-      var remote = mysql.createConnection({
-        host: config.remote.db_host,
-        user: config.remote.db_user,
-        password: config.remote.db_password,
-        database: config.remote.db_name
-      });
-
-      // Connect to remote database
-      remote.connect( function( err ) {
-
-        // Handle Errors
-        if( err ) {
-          console.log('Error connecting to remote databse:'.red, config.remote.db_name.yellow);
-          return;
-        }
-
-        // Log connection
-        console.log('Connection established to remote database:'.green, config.remote.db_name.yellow);
-
-        // Execute database comparison
-        // exec('mysqldbcompare --server1=' + config.local.db_user + '@' + config.local.db_host + ' deploylocal:deployremote --run-all-tests', function( err, out, code ) {
-        //   if ( err ) throw err;
-        //   process.stderr.write(err);
-        //   process.stdout.write(out);
-        //   process.exit(code);
-        // });
-
-      });
-
-      // Handle remote handle
-      remote.end(function(err) {
-
-        console.log('Error Connecting to Remote Database', err);
-
-      });
-
-    });
-
-  });
-
+  wordpress.deploy();
 });
 
 // --------------------------------------
-// Default Task
+// Sync Task
 // --------------------------------------
 
-gulp.task('default', function () {});
+gulp.task( 'sync', function () {
+  wordpress.sync();
+});
